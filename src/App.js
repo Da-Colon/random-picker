070d0c64a9c0ac7f3754aeecc1bc2515d70604b6
Da-Colon <dcolonantonio@gmail.com>
2020-01-10 08:39:33 -0500

    moved data out of classList to top level

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from 'react'
+import classData from './Data/Dummy.data.json'
+
+//Import Components
 import Header from "./Components/Header";
 import ClassList from "./Components/ClassList";
 import Spinner from "./Components/Spinner";
 import CalledList from "./Components/CalledList";
+
+// Import Styles
 import {
   AppWrapper,
   LeftSideBar,
   RightSideBar,
   TopBar
 } from "./Components/Styles/Containers";
-
 import './app.css'
 
 function App() {
+  const [students, setStudents] = useState([])
+
+  useEffect(() => {
+    setStudents(classData.ClassList) 
+  },[])
+  
+
+  const removeStudent = (index) => {
+    const newArray = [...students]
+    newArray.splice(index, 1)
+    setStudents(newArray)
+  }
+
   return (
     <div className="App">
       <AppWrapper>
@@ -20,7 +37,7 @@ function App() {
           <Header />
         </TopBar>
         <LeftSideBar>
-          <ClassList />
+          <ClassList students={students} removeStudent={removeStudent} />
         </LeftSideBar>
         <Spinner />
         <RightSideBar>
--- src/Components/ClassList.jsx
+++ src/Components/ClassList.jsx
@@ -1,28 +1,15 @@
 import React, {useState, useEffect} from 'react'
 import classData from '../Data/Dummy.data.json'
 
-export default function ClassList() {
-  const [students, setStudents] = useState([])
-  
-  useEffect(() => {
-    setStudents(classData.ClassList) 
-  },[])
-  
+export default function ClassList(props) {
 
-  const removeStudent = (index) => {
-    // console.log("this", students.slice((index-1), 1))
-    const newArray = [...students]
-    newArray.splice(index, 1)
-    console.log("THIS", newArray)
-    setStudents(newArray)
-  }
 
   return (
     <ul>
-      {students.map((student, index) => 
+      {props.students.map((student, index) => 
       (
         <li key={student.id}>{student.name}
-        <button onClick={() =>removeStudent(index)}>X</button>
+        <button onClick={() => props.removeStudent(index)}>X</button>
         </li>
       ))}
     </ul>
