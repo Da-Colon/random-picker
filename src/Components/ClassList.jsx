8dc7d2bab62a89ebe470c5a9861120654b770c08
Da-Colon <dcolonantonio@gmail.com>
2020-01-10 01:48:08 -0500

    added classList no styling to component

--- src/Components/ClassList.jsx
+++ src/Components/ClassList.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import classData from '../Data/Dummy.data.json'
 
 export default function ClassList() {
+  const [students, setStudents] = useState([])
+  
+  useEffect(() => {
+    setStudents(classData.ClassList) 
+  },[])
+  
+
+  const removeStudent = (index) => {
+    // console.log("this", students.slice((index-1), 1))
+    const newArray = [...students]
+    newArray.splice(index, 1)
+    console.log("THIS", newArray)
+    setStudents(newArray)
+  }
+
   return (
-    <div>
+    <ul>
+      {students.map((student, index) => 
+      (
+        <li key={student.id}>{student.name}
+        <button onClick={() =>removeStudent(index)}>X</button>
+        </li>
+      ))}
+    </ul>
       
-    </div>
   )
 }
