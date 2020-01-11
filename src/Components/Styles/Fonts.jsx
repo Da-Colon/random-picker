c907d16248f8e54b446923188c2a426290b21afa
Da-Colon <dcolonantonio@gmail.com>
2020-01-11 18:09:36 -0500

    removed animations

--- src/Components/Styles/Fonts.jsx
+++ src/Components/Styles/Fonts.jsx
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 export const PageTitle = styled.h1`
   text-align: center;
@@ -27,18 +27,9 @@ export const Li = styled.li`
   padding: 4px;
 `;
 
-const chosenName = keyframes`
-  0% {font-size: 1%; padding-top: 0%;},
-  20% {font-size: 15%; padding-top: 20%;},
-  40% {font-size: 20%; padding-top: 40%;},
-  60% {font-size: 40%; padding-top: 60%;},
-  80% {font-size: 70%; padding-top: 80%;},
-  100% {font-size: 100%; padding-top: 100%;}
-`;
 export const Name = styled.p`
   display: static;
   text-align: center;
   font-size: 3rem;
   padding-top: 100px;
-  animation-name: ${chosenName};
 `;
