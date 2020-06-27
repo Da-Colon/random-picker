0d8f2c4dbfaf959591c5de6d197c61e0b338d818
Da-Colon <dcolonantonio@gmail.com>
2020-06-26 20:33:08 -0400

    prevents user navigation when not logged in

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,45 @@
-import React from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import Home from '../containers/home'
-import Spinner from '../containers/spinner'
-import Register from '../containers/register'
-import UploadClassList from '../containers/uploadClassList'
-import EditClass from '../containers/editClass'
+import React, { useEffect, useContext } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "../containers/home";
+import Spinner from "../containers/spinner";
+import Register from "../containers/register";
+import UploadClassList from "../containers/uploadClassList";
+import EditClass from "../containers/editClass";
+import Instructions from "../containers/instructions";
+import { StateContext } from "../context";
 
 export default function AppRouter() {
+  const { user } = useContext(StateContext);
+  const pathName = window.location.pathname;
+
+  useEffect(() => {
+    if (user.id === null) {
+      switch (pathName) {
+        case "/upload":
+        case "/edit":
+        case "/spinner":
+          window.location.replace("/");
+          break;
+        default:
+          return;
+      }
+    }
+  }, [pathName, user.id]);
+
   return (
     <div className="h-screen flex justify-center">
-
-    <Router>
-      <Route path="/" component={Home} exact />
-      <Route path="/spinner" component={Spinner} exact />
-      <Route path="/register" component={Register} exact />
-      <Route path="/upload" component={UploadClassList} exact />
-      <Route path="/edit" component={EditClass} exact />
-    </Router>
+      <Router>
+        <Route path="/" component={Home} exact />
+        <Route path="/register" component={Register} exact />
+        <Route path="/instructions" component={Instructions} exact />
+        {user.id !== null && (
+          <>
+            <Route path="/spinner" component={Spinner} exact />
+            <Route path="/upload" component={UploadClassList} exact />
+            <Route path="/edit" component={EditClass} exact />
+          </>
+        )}
+      </Router>
     </div>
-  )
+  );
 }
