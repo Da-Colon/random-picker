0d1c91d69d43a350f384cad63c364d75f3e01e0b
Da-Colon <dcolonantonio@gmail.com>
2020-09-14 00:42:58 -0400

    updated primary button adding disabled style and conditional

--- src/components/views/buttons/primaryButton.js
+++ src/components/views/buttons/primaryButton.js
@@ -1,14 +1,21 @@
 import React from "react";
 import classnames from 'classnames'
 
-const base = "bg-dc-light py-1 text-md w-24 text-dc-gold tracking-wide rounded border-black border-solid border"
+const base = "py-1 text-md w-24 tracking-wide rounded border-black border-solid border"
+const disabledClass = "bg-gray-500 text-gray-700 cursor-default"
 
-const PrimaryButton =  ({label, classname, ...rest}) => (
+const PrimaryButton =  ({label, classname, disable, bgColor, textColor, ...rest}) => (
   <button
     {...rest}
-    className={classnames(base, classname)} >
+    disabled={disable}
+    className={disable ? classnames(base, disabledClass, classname) : classnames(base, classname, bgColor, textColor)} >
     {label}
   </button>
 );
+
+PrimaryButton.defaultProps = {
+  bgColor: "bg-dc-light",
+  textColor: "text-dc-gold"
+}
 
 export default PrimaryButton;
